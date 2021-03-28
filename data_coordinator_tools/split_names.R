e9e98c0a405177b74fe534dc8b3eb3e1f9c9ac30
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2021-03-28 04:26:15 -0400

    Data read-in

--- data_coordinator_tools/split_names.R
+++ data_coordinator_tools/split_names.R
@@ -1,2 +1,10 @@
+# This script will separate the first and last names of a single column into two columns
+
+#load necessary packages
 library(tidyverse)
+
+#set directories
+home <- getwd()
+data.dir <- file.path(home, "data")
+output.dir <- file.path(home, "output")
 
