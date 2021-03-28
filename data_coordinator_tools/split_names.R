153008765e3d63fa4b1979c225ff807e4dcd25bb
NaimChowdhury <naimul.chowdhury15@ncf.edu>
2021-03-28 04:38:23 -0400

    first draft

--- data_coordinator_tools/split_names.R
+++ data_coordinator_tools/split_names.R
@@ -8,3 +8,16 @@ home <- getwd()
 data.dir <- file.path(home, "data")
 output.dir <- file.path(home, "output")
 
+# read the CSV in
+data <- read_csv(file.path(data.dir, "coaching_emails.csv"))
+
+#separate the names
+first.last <- data %>% 
+  separate(, col = "Name:", 
+           into = c('first_name', 'last_name'), sep = ' ')
+
+# remove NAs
+cleaned <- first.last %>%  select(first_name, last_name, `Email address:`) %>%
+  drop_na(first_name, `Email address:`)
+
+cleaned %>% write_csv(, file = file.path(output.dir, "coaching_separated.csv"), na = "NA", append = FALSE, col_names = TRUE)
